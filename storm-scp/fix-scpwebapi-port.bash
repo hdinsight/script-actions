f63f4d28a8e289849f58634eeec3528dd64687c8
Ravi Peri <raviperi@microsoft.com>
2017-04-19 11:30:43 -0700

    Fix config file path

--- storm-scp/fix-scpwebapi-port.bash
+++ storm-scp/fix-scpwebapi-port.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
-cp /usr/hdp/current/storm-client/scpwebapi/resources/scpwebapi-humboldt.yaml /usr/hdp/current/storm-client/resources/scpwebapi-humboldt.yaml.bak
-sed -e "s/scp.webapi.ui.port: 80/scp.webapi.ui.port: 8888/g" /usr/hdp/current/storm-client/resources/scpwebapi-humboldt.yaml >/usr/hdp/current/storm-client/resources/scpwebapi-humboldt.yaml
+cp /usr/hdp/current/storm-client/scpwebapi/resources/scpwebapi-humboldt.yaml /usr/hdp/current/storm-client/scpwebapi/resources/scpwebapi-humboldt.yaml.bak
+sed -e "s/scp.webapi.ui.port: 80/scp.webapi.ui.port: 8888/g" /usr/hdp/current/storm-client/scpwebapi/resources/scpwebapi-humboldt.yaml >/usr/hdp/current/storm-client/scpwebapi/resources/scpwebapi-humboldt.yaml
