cadad8b898a753f675a285672f7e9e7595c39531
Maxim Lukiyanov <maxiluk@gmail.com>
2018-03-01 12:19:40 -0800

    Spark 2.3

---
+++ install-spark2-3/install-spark2-3-v01.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+newspark="spark-2.3.0"
+
+SPARK_DIR="$(readlink -f "/usr/hdp/current/spark2-client")"
+SPARK_CONF_DIR="$(readlink -f "/usr/hdp/current/spark2-client/conf")"
+CURRENT_DIR=${SPARK_DIR%/spark2}
+HADOOP_DIR="$CURRENT_DIR/hadoop"
+HADOOP_YARN_DIR="$CURRENT_DIR/hadoop-yarn"
+
+## Download & Install Binary
+cd "/tmp"
+curl "https://www.apache.org/dyn/closer.lua/spark/spark-2.3.0/$newspark-bin-hadoop2.7.tgz" | tar xzf -
+cd "$newspark"
+rm -r "jars/hadoop"* "conf"
+ln -s "$SPARK_CONF_DIR" "conf"
+cd ..
+rm -r "$SPARK_DIR"
+mv "$newspark" "$SPARK_DIR"
+
+# Create symlinks
+sudo ln -sfn "$SPARK_DIR/yarn/$newspark-yarn-shuffle.jar" \
+   "$HADOOP_DIR/lib/spark-yarn-shuffle.jar"
+sudo ln -sf $HADOOP_DIR /usr/hdp/current/hadoop
+sudo ln -sf $HADOOP_YARN_DIR /usr/hdp/current/hadoop-yarn   
+
+echo "$newspark installation completed"
